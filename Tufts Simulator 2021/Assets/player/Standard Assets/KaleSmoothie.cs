d6d558baf87e896053660bb31437f31fc08b1fce
avkothari07 <78499414+avkothari07@users.noreply.github.com>
2021-11-07 11:48:40 -0500

    kale smoothie

---
+++ Tufts Simulator 2021/Assets/player/Standard Assets/KaleSmoothie.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class KaleSmoothie : MonoBehaviour
+{
+    public GameObject player; 
+    private FirstPersonController fps_script;
+
+void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+        Debug.Log("hi");
+        fps_script = player.GetComponent<FirstPersonController>();
+        // GameObject thePlayer = GameObject.Find("FirstPersonCharacter");
+        // FirstPersonController script = thePlayer.GetComponent<FirstPersonController>();
+        fps_script.m_PlayerXP += 5;
+    }
+}    

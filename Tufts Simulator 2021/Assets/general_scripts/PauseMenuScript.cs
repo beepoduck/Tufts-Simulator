1a72fce30b480f598f6806e544458168eb06446a
BrettSilv12 <33107962+BrettSilv12@users.noreply.github.com>
2021-11-16 22:48:28 -0500

    added pause menu

---
+++ Tufts Simulator 2021/Assets/general_scripts/PauseMenuScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject CanvasObject;
+
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Escape))
+        {
+          PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+      //pauses game / freezes time
+      Time.timeScale = 0;
+      //lets player use mouse to click buttons on menu
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None;
+      //adds pause menu to screen
+      CanvasObject.SetActive(true);
+    }
+
+    public void PauseResumeButton()
+    {
+      //removes pause menu from screen
+      CanvasObject.SetActive(false);
+      //resumes game (unfreezes time)
+      FindObjectOfType<npc_interaction>().FreePlayer();
+    }
+
+    public void PauseQuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}

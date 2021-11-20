123433f4337bb40185163897467f11a049aaba1f
avkothari07 <78499414+avkothari07@users.noreply.github.com>
2021-11-20 10:59:40 -0500

    Update EnemyController.cs

--- Tufts Simulator 2021/Assets/general_scripts/EnemyController.cs
+++ Tufts Simulator 2021/Assets/general_scripts/EnemyController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class EnemyManager : MonoBehaviour
 {
